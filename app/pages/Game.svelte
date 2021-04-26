1ab9051780325242c8318377563f1db24d1dd0ca
amalija-ramljak <amalija.ramljak@gmail.com>
2021-04-26 13:16:23 +0200

    update game to work with modals

--- app/pages/Game.svelte
+++ app/pages/Game.svelte
@@ -6,10 +6,14 @@
   import { Template } from 'svelte-native/components'
   import RoundData from './RoundData.svelte'
   import App from '~/App.svelte'
+  import { isOpenRoundEntry, isOpenRoundDetails } from '~/stores/roundData.store'
+  import RoundDetails from './RoundDetails.svelte'
 
   export let players = []
-  export let shufflerIdx = 0
-  export let game = []
+
+  let shufflerIdx = 0
+  let game = []
+  let tappedDetailsIdx = -1
 
   $: MIPoints =
     game.length > 0
@@ -43,27 +47,37 @@
   })()
 
   function onDeleteRound(args) {
-    let delround = game[args.object.id]
+    console.log("onDeleteRound", args.object.id)
+    let delround = game.filter((item) => item.id === args.object.id)[0]
     MIPoints -= delround.mi
     VIPoints -= delround.vi
-    game = game.filter((item) => item.idx !== args.object.id)
+    game = game.filter((item) => item.id !== args.object.id)
     shufflerIdx = game.length % 4
-    players = players
-    console.log(shufflerIdx)
   }
 
-  function enterRound(args) {
-    navigate({
-      page: RoundData,
-      props: {
-        game: game,
-        players: players,
-      },
-    })
+  function enterRound() {
+    console.log("enterRound")
+    isOpenRoundEntry.set(true)
+  }
+
+  function addRound(round) {
+    console.log("addRound", round.id)
+    game = game.concat([round])
+  }
+
+  function openDetails(event) {
+    console.log(event)
+    console.log("openDetails", event.index)
+    tappedDetailsIdx = event.index
+    isOpenRoundDetails.set(true)
   }
 </script>
 
 <style>
+  .body-grid {
+    width: 100%;
+    height: 100%;
+  }
   .delete-btn {
     height: 25;
     width: 25;
@@ -95,36 +109,41 @@
 
 <page>
   <ActionBar />
-  <gridLayout class="body-grid" rows="50,50,*,70">
-    <label row="0" class="text-center">Dijeli: {players[shufflerIdx]}</label>
-    <listView
-      row="1"
-      items={[[`MI (${MIPoints})`, `VI (${VIPoints})`, 'Pobriši']]}>
-      <Template let:item>
-        <gridLayout row="1" columns="*,*,100">
-          <label col="0" text={item[0]} />
-          <label col="1" text={item[1]} />
-          <label class="text-right" col="2" text={item[2]} />
-        </gridLayout>
-      </Template>
-    </listView>
-    <listView row="2" items={game}>
-      <Template let:item>
-        <gridLayout class="round-item -separator" height="30" columns="*,*,100">
-          <label col="0" text={item.mi} />
-          <label col="1" text={item.vi} />
-          <flexboxLayout col="2" class="justify-end">
-            <label
-              class="delete-btn text-center"
-              id={item.idx}
-              on:tap={onDeleteRound}>
-              X
-            </label>
-          </flexboxLayout>
-        </gridLayout>
-      </Template>
-    </listView>
-
-    <button row="3" class="-primary" on:tap={enterRound}>Unesi bodove</button>
-  </gridLayout>
+  <absoluteLayout>
+    <gridLayout class="body-grid" rows="50,50,*,70">
+      <label row="0" class="text-center">Dijeli: {players[shufflerIdx]}</label>
+      <listView
+        row="1"
+        items={[[`MI (${MIPoints})`, `VI (${VIPoints})`, 'Pobriši']]} >
+        <Template let:item>
+          <gridLayout row="1" columns="*,*,100">
+            <label col="0" text={item[0]} />
+            <label col="1" text={item[1]} />
+            <label class="text-right" col="2" text={item[2]} />
+          </gridLayout>
+        </Template>
+      </listView>
+      <listView row="2" items={game} on:itemTap={openDetails}>
+        <Template let:item>
+          <gridLayout class="round-item -separator" height="30" columns="*,*,100">
+            <label col="0" text={item.mi} />
+            <label col="1" text={item.vi} />
+            <flexboxLayout col="2" class="justify-end">
+              <label
+                class="delete-btn text-center"
+                id={item.id}
+                on:tap={onDeleteRound}>
+                X
+              </label>
+            </flexboxLayout>
+          </gridLayout>
+        </Template>
+      </listView>
+      <button row="3" class="-primary" on:tap={enterRound}>Unesi bodove</button>
+    </gridLayout>
+    <RoundData newId={game.length > 0 ? game[game.length-1].id + 1 : 0} onAddRound={addRound} />
+    {#if tappedDetailsIdx > -1}
+    <RoundDetails details={game[tappedDetailsIdx].raw} />
+    {/if}
+  </absoluteLayout>
 </page>
